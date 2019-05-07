fe0a82b95a4bb71ee15e6ce52f4c0328d47dcab3
nick-moffitt <nicksmoffitt@gmail.com>
2019-05-07 11:18:04 -0600

    Fixed bug with add1t and added in documentation for text_boxes argument

--- R/add1s.R
+++ R/add1s.R
@@ -4,6 +4,7 @@
 #' Add 1 Blank PowerPoint slide
 #'
 #' This function adds a new PowerPoint slide along with a title and commentary box to the current pp object in R
+#' @param text_boxes DEFAULT = T; Automatically adds title and commentary boxes to the blank slide. Set to F for no text boxes
 #' @param slide_name DEFAULT: "Findings / 1 chart"; The name of the type of the PP slide you want added to the PP
 #' @param master_name DEFAULT: "Office Theme"; The name of the PP master layout that the slide_name comes from
 #' @keywords powerpoint slide
