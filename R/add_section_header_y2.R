119274a2b71a630bd6260e61476ae4ad5324e745
Canyen Heimuli <46830067+canyenheimuli@users.noreply.github.com>
2024-05-01 09:38:11 -0400

    Update add_section_header_y2.R
    Updates for automating municipal style reports

--- R/add_section_header_y2.R
+++ R/add_section_header_y2.R
@@ -4,9 +4,10 @@
 #'
 #' This function adds a new PowerPoint section header along with a title box
 #' @param title DEFAULT: "Title"; Add a title in quotes here to have your Section Headers put in for you by R
-#' @param n DEFAULT: "1"; The number added to your section header. If section 2, put "2", etc.
-#' @param slide_name DEFAULT: "Section Header"; The name of the type of the PP slide you want added to the PP
-#' @param master_name DEFAULT: "Office Theme"; The name of the PP master layout that the slide_name comes from
+#' @param n DEFAULT: "1"; The number added to your section header (for Qualtrics style reports only). If section 2, put "2", etc.
+#' @param report_style DEFAULT: "qualtrics"; The report style/template you are using -- must be either 'qualtrics' or 'municipal'
+#' @param slide_name DEFAULT: "Section Header; The name of the type of the PP slide you want added to the PP
+#' @param master_name DEFAULT: NULL; The name of the PP master layout that the slide_name comes from. If no argument provided and the report style is 'qualtrics', defaults to "1_Office Theme"; if no argument provided and the report style is 'municipal', defaults to "Office Theme".
 #' @param text_boxes DEFAULT = TRUE; Automatically adds a title and section number. Set to FALSE for no text boxes
 #' @keywords powerpoint slide
 #' @export
@@ -21,96 +22,147 @@
 #' }
 
 add_section_header_y2 <- function(
-  title = 'Title',
-  n = '1',
-  slide_name = "XM Sidebar (1)",
-  master_name = "1_Office Theme",
-  text_boxes = TRUE
+    title = 'Title',
+    n = '1',
+    report_style = c('qualtrics', 'municipal'),
+    master_name = NULL,
+    slide_name = NULL,
+    text_boxes = TRUE
 ) {
+  
+  # Test matching arguments
+  report_style <- rlang::arg_match(report_style)
+  
+  # Null (default) master names
+  if (report_style == 'qualtrics' & is.null(master_name)) {
+    master_name <- '1_Office Theme'
+  } else if (report_style == 'municipal' & is.null(master_name)) {
+    master_name <- 'Office Theme'
+  }
+  
+  # Null (default) section header names
+  if (report_style == 'qualtrics' & is.null(slide_name)) {
+    slide_name <- 'XM Sidebar (1)'
+  } else if (report_style == 'municipal' & is.null(slide_name)) {
+    slide_name <- 'Section Header'
+  }
+  
+  # New slide with section header text
   doc <- officer::add_slide(
     doc,
     layout = slide_name,
     master = master_name
   )
-
+  
   if (text_boxes == TRUE) {
-  doc <- doc %>% add_number_section_header(n)
-  doc <- doc %>% add_title_section_header(title)
-  } else {
-    doc <- doc
+    
+    if (report_style == 'qualtrics') {
+      
+      doc <- doc %>% add_number_section_header(n)
+      doc <- doc %>% add_title_section_header(title, report_style)
+      
+    } else {
+      
+      doc <- doc %>% add_title_section_header(title, report_style)
+      
+    }
+    
   }
+  
 }
-
 
 
 # Private functions -------------------------------------------------------
 
 add_number_section_header <- function(
-  doc,
-  text,
-  text_color = "white",
-  font_size = 139,
-  font_family = "BentonSans Regular",
-  left = .85,
-  top = 0.27,
-  width = 2,
-  height = 2
+    doc,
+    text
 ){
+  
   properties <- officer::fp_text(
-    color = text_color,
-    font.size = font_size,
-    font.family = font_family,
+    color = 'white',
+    font.size = 139,
+    font.family = 'BentonSans Regular',
     bold = TRUE
   )
-
+  
   slide_title <- officer::ftext(
     text,
     properties
   )
-
+  
   officer::ph_with(
     doc,
     value = officer::fpar(slide_title),
     location = officer::ph_location(
-      left = left,
-      top = top,
-      width = width,
-      height = height
+      left = 0.85,
+      top = 0.27,
+      width = 2,
+      height = 2
     )
   )
+  
 }
 
 add_title_section_header <- function(
-  doc,
-  text,
-  text_color = "Black",
-  font_size = 60,
-  font_family = "BentonSans Regular",
-  left = 2.55,
-  top = .75,
-  width = 11,
-  height = 1
-){
-  properties <- officer::fp_text(
-    color = text_color,
-    font.size = font_size,
-    font.family = font_family
-  )
-
-  slide_subtitle <- officer::ftext(
-    text,
-    properties
-  )
-
-  officer::ph_with(
     doc,
-    value = officer::fpar(slide_subtitle),
-    location = officer::ph_location(
-      left = left,
-      top = top,
-      width = width,
-      height = height
+    text,
+    report_style
+){
+  
+  if (report_style == 'qualtrics') {
+    
+    properties <- officer::fp_text(
+      color = 'black',
+      font.size = 60,
+      font.family = 'BentonSans Regular'
     )
-  )
+    
+    slide_subtitle <- officer::ftext(
+      text,
+      properties
+    )
+    
+    officer::ph_with(
+      doc,
+      value = officer::fpar(slide_subtitle),
+      location = officer::ph_location(
+        left = 2.55,
+        top = 0.75,
+        width = 11,
+        height = 1
+      )
+    )
+    
+  } else {
+    
+    properties <- officer::fp_text(
+      color = 'white',
+      font.size = 66,
+      font.family = 'Flama Medium'
+    )
+    
+    slide_subtitle <- officer::ftext(
+      text,
+      properties
+    )
+    
+    officer::ph_with(
+      doc,
+      value = officer::fpar(
+        slide_subtitle,
+        fp_p = fp_par(
+          text.align = 'center'
+        )
+      ),
+      location = officer::ph_location(
+        left = 1,
+        top = 3.14,
+        width = 11.34,
+        height = 1.21
+      )
+    )
+    
+  }
+  
 }
-
