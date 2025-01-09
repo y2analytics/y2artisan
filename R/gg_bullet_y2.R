235bda59a04ae1dfb3846e15ab5d20e78b48219a
Canyen Heimuli <46830067+canyenheimuli@users.noreply.github.com>
2025-01-09 16:06:53 -0500

    Update gg_bullet_y2.R
    Added "overwrite_breaks" argument

--- R/gg_bullet_y2.R
+++ R/gg_bullet_y2.R
@@ -27,6 +27,7 @@
 #' @param legend_title_size DEFAULT = 8
 #' @param legend_title DEFAULT = '', If you put in a title, the legend will default to 'top' unless otherwise specified
 #' @param nudge DEFAULT = 0; however, nudge automatically adjusts based on the max value of 'result', in most cases fitting the chart perfectly
+#' @param overwrite_breaks DEFAULT = TRUE, Whether to overwrite existing linebreaks ("\n") in string label inputs when performing text pre-processing, such as string wrapping
 #' @param title_label DEFAULT = ''; Add your title in "" as the title of the chart.
 #' @param title_size DEFAULT = 18
 #' @param x_label,y_label DEFAULT = ''; Title for the x_axis or y_axis
@@ -72,6 +73,7 @@ gg_bullet_y2 <- function(
   legend_title_size = 8,
   legend_title = '',
   nudge = 0, # auto-fills
+  overwrite_breaks = TRUE,
   title_label = '',
   title_size = 14,
   x_label = '',
@@ -208,7 +210,7 @@ if (
     ) +
     ggplot2::scale_x_discrete(
       labels = function(x) lapply(
-        strwrap(
+        strwrap_mod(
           x,
           width = label_length,
           simplify = FALSE
@@ -220,4 +222,114 @@ if (
     cond_direction
 }
 
+# Private functions -------------------------------------------------------
 
+strwrap_mod <- function (
+    x, 
+    width = 0.9 * getOption("width"),
+    overwrite_breaks = overwrite_breaks,
+    indent = 0, 
+    exdent = 0, 
+    prefix = "", 
+    simplify = TRUE, 
+    initial = prefix
+) {
+  
+  if (!is.character(x)) 
+    x <- as.character(x)
+  indentString <- strrep(" ", indent)
+  exdentString <- strrep(" ", exdent)
+  y <- list()
+  enc <- Encoding(x)
+  x <- enc2utf8(x)
+  if (any(ind <- !validEnc(x))) 
+    x[ind] <- iconv(x[ind], "UTF-8", "UTF-8", sub = "byte")
+  
+  if (overwrite_breaks) {
+    z <- lapply(strsplit(x, "\n[ \t\n]*\n", perl = TRUE), strsplit, 
+                "[ \t\n]", perl = TRUE)
+  } else {
+    z <- lapply(strsplit(x, "\n[ \t\n]*\n", perl = TRUE), strsplit, 
+                "[ \t]", perl = TRUE)
+  }
+  
+  for (i in seq_along(z)) {
+    yi <- character()
+    for (j in seq_along(z[[i]])) {
+      words <- z[[i]][[j]]
+      nc <- nchar(words, type = "w")
+      if (anyNA(nc)) {
+        nc0 <- nchar(words, type = "b")
+        nc[is.na(nc)] <- nc0[is.na(nc)]
+      }
+      if (any(nc == 0L)) {
+        zLenInd <- which(nc == 0L)
+        zLenInd <- zLenInd[!(zLenInd %in% (grep("[.?!][)\"']{0,1}$", 
+                                                words, perl = TRUE, useBytes = TRUE) + 1L))]
+        if (length(zLenInd)) {
+          words <- words[-zLenInd]
+          nc <- nc[-zLenInd]
+        }
+      }
+      if (!length(words)) {
+        yi <- c(yi, "", initial)
+        next
+      }
+      currentIndex <- 0L
+      lowerBlockIndex <- 1L
+      upperBlockIndex <- integer()
+      lens <- cumsum(nc + 1L)
+      first <- TRUE
+      maxLength <- width - nchar(initial, type = "w") - 
+        indent
+      while (length(lens)) {
+        k <- max(sum(lens <= maxLength), 1L)
+        if (first) {
+          first <- FALSE
+          maxLength <- width - nchar(prefix, type = "w") - 
+            exdent
+        }
+        currentIndex <- currentIndex + k
+        if (nc[currentIndex] == 0L) 
+          upperBlockIndex <- c(upperBlockIndex, currentIndex - 
+                                 1L)
+        else upperBlockIndex <- c(upperBlockIndex, currentIndex)
+        if (length(lens) > k) {
+          if (nc[currentIndex + 1L] == 0L) {
+            currentIndex <- currentIndex + 1L
+            k <- k + 1L
+          }
+          lowerBlockIndex <- c(lowerBlockIndex, currentIndex + 
+                                 1L)
+        }
+        if (length(lens) > k) 
+          lens <- lens[-seq_len(k)] - lens[k]
+        else lens <- NULL
+      }
+      nBlocks <- length(upperBlockIndex)
+      s <- paste0(c(initial, rep.int(prefix, nBlocks - 
+                                       1L)), c(indentString, rep.int(exdentString, nBlocks - 
+                                                                       1L)))
+      initial <- prefix
+      for (k in seq_len(nBlocks)) s[k] <- paste0(s[k], 
+                                                 paste(words[lowerBlockIndex[k]:upperBlockIndex[k]], 
+                                                       collapse = " "))
+      yi <- c(yi, s, prefix)
+    }
+    y <- if (length(yi)) 
+      c(y, list(yi[-length(yi)]))
+    else c(y, "")
+  }
+  if (length(pos <- which(enc == "latin1"))) {
+    y[pos] <- lapply(y[pos], function(s) {
+      e <- Encoding(s)
+      if (length(p <- which(e == "UTF-8"))) 
+        s[p] <- iconv(s[p], "UTF-8", "latin1", sub = "byte")
+      s
+    })
+  }
+  if (simplify) 
+    y <- as.character(unlist(y))
+  y
+  
+}
